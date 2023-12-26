cd4894d25c3929c7306819a7c6a7e9be23f82592
ViniMagaa <vinisantos2008vs@gmail.com>
2023-12-26 20:16:15 -0300

    introdução ao C#

---
+++ exercicios-de-aula/aula-dia-08-12-introducao-c-sharp/aula_1/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_1
+{
+    internal class Pessoa
+    {
+        public Pessoa(string nome)
+        {
+            this.Nome = nome;
+        }
+        public string Nome { get; set; }
+    }
+}
---
+++ exercicios-de-aula/aula-dia-08-12-introducao-c-sharp/aula_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using aula_1;
+
+int n1, n2;
+
+Console.Write("Digite o primeiro número: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+int soma = n1 + n2;
+
+Console.WriteLine("A soma desses números é: " + soma);
+
+Console.Write("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+var pessoa = new Pessoa(nome);
+
+Console.WriteLine("Seu nome é " + pessoa.Nome);
